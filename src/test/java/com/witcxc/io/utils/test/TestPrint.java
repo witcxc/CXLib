24255238697492b976609eef4b2aea1cfc6edba7
陈喜(技术中心) <witcxcchen@sohu-inc.com>
2014-01-06 01:44:45 +0800

    print

---
+++ src/test/java/com/witcxc/io/utils/test/TestPrint.java
@@ -0,0 +1,15 @@
+package com.witcxc.io.utils.test;
+
+import static com.witcxc.io.utils.Print.*;
+
+public class TestPrint {
+
+	public static void main(String args[]) {
+
+		print("Hello");
+		print();
+		printnb("a");
+		printnb("b");
+	}
+
+}
