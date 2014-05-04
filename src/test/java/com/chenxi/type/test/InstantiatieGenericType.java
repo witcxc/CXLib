c6147a79463150bff82fe9187c98bf12d706cc7b
陈喜(技术中心) <witcxcchen@sohu-inc.com>
2014-05-04 15:21:57 +0800

    Erase

---
+++ src/test/java/com/chenxi/type/test/InstantiatieGenericType.java
@@ -0,0 +1,30 @@
+package com.chenxi.type.test;
+import static com.witcxc.io.utils.Print.*;
+
+class ClassAsFactory<T>{
+	T x;
+	public ClassAsFactory(Class<T> kind){
+		try{
+			x = kind.newInstance();
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+}
+class Employee{}
+public class InstantiatieGenericType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassAsFactory<Employee> cf = new ClassAsFactory<Employee>(Employee.class);
+		print("create employ succeeded!");
+		try{
+		ClassAsFactory<Integer> cfi = new ClassAsFactory<Integer>(Integer.class);
+		}catch(Exception e){
+			print("create integer failed"+e);
+		}
+		Integer i = new Integer(1);
+		print(i);
+	}
+
+}
